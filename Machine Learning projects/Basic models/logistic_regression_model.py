563ec0173c2f659e34bac718e6e49517f2dbccaf
dev-msk5 <110525758+dev-msk5@users.noreply.github.com>
2025-09-05 22:25:50 +0200

    Started logistic regression
    Also modified w to W, using the mathematical convention for matrices

--- Machine Learning projects/Basic models/logistic_regression_model.py
+++ Machine Learning projects/Basic models/logistic_regression_model.py
@@ -13,3 +13,37 @@ def generate_logistic_data(n_samples=100):
     probs = 1 / (1 + np.exp(-logits))
     y = (probs > 0.5).astype(int)
     return X, y, w, b
+
+#Logistic regression model
+
+X, y, true_W, true_b = generate_logistic_data()
+W = np.ones((X.shape[1],1))
+b = 1
+m,n = X.shape
+
+def sigmoid(X,W,b):
+    z = np.dot(X,W) + b
+    fwb = (1 / (1 + np.exp(-z)))
+    return fwb
+# print(sigmoid(X,W,b).shape)
+
+def cost_function(X,y,W,b):
+    fwb = sigmoid(X,W,b)        # (100, 1) , same shape as y
+    return np.sum(y*np.log(fwb)+(1-y)*np.log(1-fwb)) / - m     # cost function using simplified definition
+
+# print(cost_function(X,y,W,b))
+
+def gradient_function(X,y,W,b):
+    
+    dj_dW = np.zeros(n)
+    dj_db = 0
+    
+    error = sigmoid(X,W,b) - y
+    dj_dW = (np.dot(X.T,error) / m)
+    dj_db = (np.sum(error) / m)
+    
+    return dj_dW, dj_db
+    
+
+def logistic_regression_model(X,y,W,b):
+    pass
--- Machine Learning projects/Basic models/multiple_regression_model.py
+++ Machine Learning projects/Basic models/multiple_regression_model.py
@@ -4,69 +4,69 @@ import math
 import copy
 
 
-# Multiple linear regression dataset (y = Xw + b + noise)
+# Multiple linear regression dataset (y = XW + b + noise)
 def generate_multiple_linear_data(n_samples=100, n_features=4, noise=1.05):
     X = np.random.rand(n_samples, n_features) * 10
-    true_w = np.random.randn(n_features, 1)
+    true_W = np.random.randn(n_features, 1)
     b = np.random.randn(1)
-    y = X @ true_w + b + np.random.randn(n_samples, 1) * noise
-    return X, y, true_w, b
+    y = X @ true_W + b + np.random.randn(n_samples, 1) * noise
+    return X, y, true_W, b
 
 
 # Multiple Variable Linear Regression
-X, y, true_w, true_b =generate_multiple_linear_data()
+X, y, true_W, true_b =generate_multiple_linear_data()
 print(f"X Shape: {X.shape},  \n{X[:5]}")
 print(f"y shape: {y.shape},  \n{y[:5]}")
 
 b=100
 n_features = X.shape[1]   # number of features
-w = np.full((n_features, 1), 10.0)     # making weight vector = 10 for every element
+W = np.full((n_features, 1), 10.0)     # making weight vector = 10 for every element
 alfa = 0.015
 iterations = 8000
 
 
-def compute_cost(X,y,w,b):  # cost function 
+def compute_cost(X,y,W,b):  # cost function 
     m = X.shape[0]
     
-    fwb= np.dot(X,w)+b
+    fwb= np.dot(X,W)+b
     cost = (np.sum((fwb - y) ** 2)/ (2 * m))
     
     return cost
 
-def gradient_function(X,y,w,b): # gradient function
-    m,n = X.shape   # m rows or examples , n colums or features
+def gradient_function(X,y,W,b): # gradient function
+    m,n = X.shape   # m roWs or examples , n colums or features
     
-    dj_dw=np.zeros((n,))
+    dj_dW=np.zeros((n,))
     dj_db=0
     
-    fwb = np.dot(X,w)+b
+    fwb = np.dot(X,W)+b
     error = (fwb-y)
-    dj_dw = (np.dot(X.T,error)/m)   # n x 1 column vector
+    dj_dW = (np.dot(X.T,error)/m)   # n x 1 column vector
     dj_db = (np.sum(error)/m)       # 1 x 1 scalar
     
-    return dj_dw, dj_db
+    return dj_dW, dj_db
 
-def multiple_linear_model(X,y,w,b,alfa,iterations):
+def multiple_linear_model(X,y,W,b,alfa,iterations):
     print(X[:5])
     print(y[:5])
-    w = copy.deepcopy(w)
-    w_history=[]
+    W = copy.deepcopy(W)
+    W_history=[]
     J_history=[]
     
     
     for i in range(iterations):                 # - Implementing gradient descent -
         
-        dj_dw, dj_db =gradient_function(X,y,w,b)                
+        dj_dW, dj_db =gradient_function(X,y,W,b)                
         b -= alfa * dj_db
-        w -= alfa * dj_dw                                            # - 
+        W -= alfa * dj_dW                                            # - 
     
         if i<10000:                         # Stopping if too many iterations
-            cost=compute_cost(X,y,w,b)
+            cost=compute_cost(X,y,W,b)
         else:
             break
         
-        if i% math.ceil(iterations/10) == 0:            # Saving data / 10% steps
-            w_history.append(w)
+        if i% math.ceil(iterations/20) == 0:            # Saving data / 5% steps
+            W_history.append(W)
             J_history.append(cost)
             print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f} ")
     
@@ -74,7 +74,7 @@ def multiple_linear_model(X,y,w,b,alfa,iterations):
     # print(f"{'Final bias':>15} | {'True bias':>15}")
     # print(f"{float(b):15.4f} | {float(true_b):15.4f}\n")
     print(f"{'Final weights':>15} | {'True weights':>15}")
-    for fw, tw in zip(w.flatten(), true_w.flatten()):
+    for fw, tw in zip(W.flatten(), true_W.flatten()):
         print(f"{fw:15.4f} | {tw:15.4f}")
     # Plot the linear fit
     plt.plot(range(len(J_history)), J_history, c="b")
@@ -84,6 +84,6 @@ def multiple_linear_model(X,y,w,b,alfa,iterations):
     plt.show()
     
 
-    return w, b, J_history, w_history
+    return W, b, J_history, W_history
 
-multiple_linear_model(X,y,w,b,alfa,iterations)
+multiple_linear_model(X,y,W,b,alfa,iterations)
