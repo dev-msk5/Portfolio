d4812059a6c9b31d901fba0b5e5fbdaf460713e4
dev-msk5 <110525758+dev-msk5@users.noreply.github.com>
2025-11-03 22:53:37 +0100

    Space Titanic starters

---
+++ Kaggle/Competitions/Space titanic/space_titanic.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from pathlib import Path
+
+BASE = Path(__file__).resolve().parent
+
+train = pd.read_csv(BASE/"train.csv")
+test = pd.read_csv(BASE/"test.csv")
+
+#Cleaning
+features = ["HomePlanet","CryoSleep","Destination","Age","VIP","RoomService","FoodCourt","ShoppingMall","Spa","VRDeck"]
+train_x_df = train[features].copy()
+train_y_df = train["Transported"].copy()
+
+#Making categorical numeric data
+train_x_df["VIP"] = train_x_df["VIP"].map({'False':0, 'True':1})
+print(train_x_df["HomePlanet"].unique())
+train_x_df["CryoSleep"] = train_x_df["CryoSleep"].map({'False':0, 'True':1})
+train_x_df["HomePlanet"] = train_x_df["HomePlanet"].map({'Europa':0, 'Earth':1,'Mars':2})
+print(train_x_df["HomePlanet"].unique())
+
+
+
+# print(test.head())
+# print(train.head())
