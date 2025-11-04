f464b1a4e610eefc672424bc9ddeef3827253a10
dev-msk5 <110525758+dev-msk5@users.noreply.github.com>
2025-11-04 10:38:32 +0100

    Additional cleaning

--- Kaggle/Competitions/Space titanic/space_titanic.py
+++ Kaggle/Competitions/Space titanic/space_titanic.py
@@ -11,7 +11,7 @@ train = pd.read_csv(BASE/"train.csv")
 test = pd.read_csv(BASE/"test.csv")
 
 #Cleaning
-features = ["HomePlanet","CryoSleep","Destination","Age","VIP","RoomService","FoodCourt","ShoppingMall","Spa","VRDeck"]
+features = ["HomePlanet","CryoSleep","Cabin","Destination","Age","VIP","RoomService","FoodCourt","ShoppingMall","Spa","VRDeck"]
 train_x_df = train[features].copy()
 train_y_df = train["Transported"].copy()
 
@@ -20,7 +20,11 @@ train_x_df["VIP"] = train_x_df["VIP"].map({'False':0, 'True':1})
 print(train_x_df["HomePlanet"].unique())
 train_x_df["CryoSleep"] = train_x_df["CryoSleep"].map({'False':0, 'True':1})
 train_x_df["HomePlanet"] = train_x_df["HomePlanet"].map({'Europa':0, 'Earth':1,'Mars':2})
-print(train_x_df["HomePlanet"].unique())
+train_x_df["Destination"] = train_x_df["Destination"].map({'TRAPPIST-1e':0, 'PSO J318.5-22':1,'55 Cancri e':2})
+print(train_x_df["Cabin"].unique())
+
+train_y_df["Transported"] = train_y_df["Transported"].map({'False':0, 'True':1})
+
 
 
 
