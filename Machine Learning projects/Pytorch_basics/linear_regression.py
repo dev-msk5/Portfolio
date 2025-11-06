f5b26bf9c85d968eb7a54e8ee2acc826ec9140bf
dev-msk5 <110525758+dev-msk5@users.noreply.github.com>
2025-11-06 13:43:23 +0100

    Implemented basic model

--- Machine Learning projects/Pytorch_basics/linear_regression.py
+++ Machine Learning projects/Pytorch_basics/linear_regression.py
@@ -1,1 +1,21 @@
+import torch
+from torch import nn
 
+weight_setup = 0.7
+bias_setup = 2
+data = torch.randn(50)
+y_setup = weight_setup * data + bias_setup
+
+# print(data)
+X_train, y_train = data[:len(data)*0.8], y_setup[:len(data)*0.8]
+X_test,y_test = data[len(data)*0.8:], y_setup[len(data)*0.8:]
+
+# Linear regression model
+class Linear_Regression_Model():
+    def __init__(self):
+        super.__init__()
+        self.weights = nn.Parameter(torch.randn(1, requires_grad=True, dtype=float))
+        self.bias = nn.Parameter(torch.randn(1, requires_grad=True, dtype=float))
+        
+    def forward(self, X):
+        return self.weights * X + self.bias
